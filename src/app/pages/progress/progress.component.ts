df603e89d24ca5935a01703684364c94b42a1cdd
jordi Olmedillas <jordi.olmedillas@gmail.com>
2020-03-30 16:46:44 +0200

    progres.component edited

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-progress',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+  progreso: number = 25;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor > 0) {
+      this.progreso = 100;
+      return;
+    }
+    if (this.progreso < 0 && valor < 0) {
+    // if (this.progreso <= 0 ) {
+      this.progreso = 0;
+      return;
+    }
+    this.progreso = Number(this.progreso) + valor;
+  }
 }
